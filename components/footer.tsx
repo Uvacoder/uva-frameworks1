8eb6933b92a1e0ed19e6c3a1048e51e8c092c108
Uvacoder <uva22940coder@gmail.com>
2022-10-22 03:34:18 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = (props) => (
   <div className="footer">
     <div className="text-footer">
-      © 2020 - 2021 | {" "}
+      © 2020 - 2022 | {" "}
       <Link href="/privacy">
         <a>Privacy</a>
       </Link>{" "}
@@ -12,7 +12,7 @@ const Footer = (props) => (
       </Link>
       <br></br>
     </div>
-    <a href="https://github.com/swve" target="_blank" rel="noreferrer">
+    <a href="https://github.com/uvacoder" target="_blank" rel="noreferrer">
       <img alt="Sweave logo" src="../img/sweave.png" />
     </a>
     <br></br>
